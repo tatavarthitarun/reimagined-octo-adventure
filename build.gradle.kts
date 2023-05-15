400455c743ff778eb0ac1c5e046b77ac4c41f501
tatavarthitarun <tatavarthitarun@gmail.com>
2023-05-15 11:20:23 +0530

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    id("com.android.application") version "8.0.1" apply false
+    id("com.android.library") version "8.0.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.21" apply false
+    id("com.google.devtools.ksp") version "1.8.21-1.0.11" apply false
+}
+
+buildscript {
+    val hiltVersion by extra("2.46.1")
+
+    dependencies {
+        classpath("com.google.dagger:hilt-android-gradle-plugin:$hiltVersion")
+    }
+}// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
